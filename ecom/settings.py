848d449361e800d24d45ce120cf79a79dad8cb94
Your Name <you@example.com>
2017-12-13 16:37:17 +0530

    db

--- ecom/settings.py
+++ ecom/settings.py
@@ -77,24 +77,6 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'ecom.urls'
 
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-                'social_django.context_processors.backends',
-                'social_django.context_processors.login_redirect',
-                'django.template.context_processors.media',
-            ],
-        },
-    },
-]
 
 WSGI_APPLICATION = 'ecom.wsgi.application'
 
